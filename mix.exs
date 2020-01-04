1d8aaee2a3e3fe2f5b606ef38508bbaccc99374a
nonblockio <nonblockio@icloud.com>
2020-01-04 12:55:11 +0800

    Add publish info

--- mix.exs
+++ mix.exs
@@ -1,13 +1,23 @@
 defmodule EctoTransit.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @name "EctoTransit"
+
   def project do
     [
       app: :ecto_transit,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      name: @name,
+      docs: [
+        main: @name,
+        source_url: "https://github.com/nonblockio/ecto_transit"
+      ]
     ]
   end
 
@@ -18,7 +28,20 @@ defmodule EctoTransit.MixProject do
   defp deps do
     [
       {:ecto, ">= 3.0.0"},
-      {:ecto_enum, ">= 1.2.0", only: :test}
+      {:ecto_enum, ">= 1.2.0", only: :test},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description do
+    "A transition validator for Ecto and EctoEnum."
+  end
+
+  defp package do
+    [
+      maintainers: ["nonblockio"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/nonblockio/ecto_transit"}
     ]
   end
 end
